8710fdc45b6b566f547c3c1200ac05654dc1822d
fjokuns <fejirookunsuwa@gmail.com>
2022-10-02 21:05:44 +0100

    deploy

--- chaw/settings.py
+++ chaw/settings.py
@@ -147,8 +147,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'okunsuwafejiro@gmail.com'
-EMAIL_HOST_PASSWORD = 'lmtlrwyvhzxfekxb'
+# EMAIL_HOST_USER = 'okunsuwafejiro@gmail.com'
+EMAIL_HOST_USER = 'bigboyokuns@gmail.com'
+EMAIL_HOST_PASSWORD = 'ecgmnawuvdhjxgpo'
+# EMAIL_HOST_PASSWORD = 'lmtlrwyvhzxfekxb'
 EMAIL_USE_TLS = True
 #email setting done 
 
--- techbro/views.py
+++ techbro/views.py
@@ -65,7 +65,8 @@ def password_reset_request(request):
                 email_template_name = "password/password_reset_email.txt"
                 c = {
                     "email":user.email,
-                    'domain':'127.0.0.1:8000',
+                    # 'domain':'127.0.0.1:8000',
+                    'domain':'54.83.87.115',
                     'site_name': 'Refill',
                     "uid": urlsafe_base64_encode(force_bytes(user.pk)),
                     "user": user,
